49873b0a3fc2af57263696d3f5ae0bfc0bff7153
Helena Octaviano <helena.petrenis@edu.senai.br>
2026-05-29 13:28:39 -0300

    estrutura inicial do SCES

---
+++ sces.py
@@ -0,0 +1,12 @@
+## Definindo as variáveis
+Matriz_Produtos = [
+    [], ## ID do Produto (numérico)
+    [""], ## Nome do Produto (texto)
+    [], ## Quantidade em Estoque (numérico)
+    [""] ## Localização (texto)
+]
+
+print("\n------Sistema de Controle de Estoque Simplificado (SCES)-----")
+
+print("\n--Escolha a opção desejada:")
+input(f"| 1. Adicionar Produto | 2. Listar Todos os Produtos | 3. Buscar Produto por ID | 4. Atualizar Estoque | 5. Sair do Programa |")
